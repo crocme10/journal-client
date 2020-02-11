f4b7c06f07595f47a00df25b3e292e8849c1a963
riendegris <matt@area403.org>
2020-02-11 11:50:24 +0100

    Simple Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template>
-  <div id="app" class="font-text">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <!-- outside container, purpose is to have the inner div centered -->
+  <div id="app" class="flex items-center justify-center">
+    <!-- this div is used to set the width of the display relative to the size of the display -->
+    <div class="font-text sm:w-5/6 md:w-4/5 lg:w-3/4">
+      <Navbar />
+      <router-view />
     </div>
-    <router-view/>
   </div>
 </template>
+
+<script>
+import Navbar from '@/components/Navbar'
+
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <header class='bg-gray-900 sm:flex sm:justify-between sm:items-center sm:px-4 sm:py-3 font-header'>
+    <div class='flex items-center justify-between px-4 py-3 sm:p-0'>
+      <div>
+        <!--<img class='h-8' src='/img/logo-inverted.svg' alt='Workcation'>-->
+        <h4 class='text-gray-500'>Dwellings</h4>
+      </div>
+      <div class='sm:hidden'>
+        <button @click='isOpen = !isOpen' type='button' class='block text-gray-500 hover:text-white focus:text-white focus:outline-none'>
+          <svg class='h-6 w-6 fill-current' viewBox='0 0 24 24'>
+            <path v-if='isOpen' fill-rule='evenodd' d='M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z'/>
+            <path v-if='!isOpen' fill-rule='evenodd' d='M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z'/>
+          </svg>
+        </button>
+      </div>
+    </div>
+    <div :class='isOpen ? "block" : "hidden"' class='px-2 pt-2 pb-4 sm:flex sm:p-0'>
+      <a href='#' class='block text-gray-500 font-semibold px-2 py-1 rounded hover:bg-gray-800'>About</a>
+      <a href='#' class='mt-1 block text-gray-500 font-semibold px-2 py-1 rounded hover:bg-gray-800 sm:mt-0 sm:ml-2'>Login</a>
+      <a href='#' class='mt-1 block text-gray-500 font-semibold px-2 py-1 rounded hover:bg-gray-800 sm:mt-0 sm:ml-2'>Contact</a>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'navbar',
+  data: () => ({
+    isOpen: false
+  })
+}
+</script>
